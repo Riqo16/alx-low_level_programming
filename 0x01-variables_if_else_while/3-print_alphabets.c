d05e3fa46b5f5d0dfe5f931737245d6dc9323887
Riqo16 <ridwan.omotayo@gmail.com>
2022-12-09 14:11:08 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -17,7 +17,7 @@ int main(void)
 			putchar(l);
 			l++;
 		}
-	while 
+	while
 		(u <= 'Z') {
 			putchar(u);
 			u++;

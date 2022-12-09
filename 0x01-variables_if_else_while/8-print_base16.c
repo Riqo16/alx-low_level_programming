038a10d60ab0539b5030fae58aa9df7ae6bae079
Riqo16 <ridwan.omotayo@gmail.com>
2022-12-09 15:39:47 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	char b;
+
+	a = 0;
+	b ='a';
+	while 
+		(a < 10) {
+			putchar(a +'0');
+			a++;
+		}
+	while 
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}

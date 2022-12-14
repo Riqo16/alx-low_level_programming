317fc7feef78ec1d8b8aaaecdfbe723849595209
Riqo16 <ridwan.omotayo@gmail.com>
2022-12-14 03:08:20 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check main
+ * @a: An interger a
+ * @b: An interger b
+ * Description: Function that adds two intergers and returns the ruselt
+ * Return: Resutl to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
